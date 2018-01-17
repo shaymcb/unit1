921831dcccf254e2680fa646edb48603de422d13
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-01-17 08:21:25 -0500

    Updated from Brython Server: 1/17/2018 8:21:25 AM

--- face.py
+++ face.py
@@ -1,1 +1,10 @@
+#Shaylee McBride
+#17Jan2018
+#face.py - draws a creepy face
 
+print('   xxxxx')
+print('  x     x')
+print('((  o o  ))')
+print('  |     |')
+print('  | === |')
+print('   -----')
