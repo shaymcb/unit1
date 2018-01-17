ff584669c2963c9a648b8f2e60a18a2534baf736
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-01-17 08:33:47 -0500

    Updated from Brython Server: 1/17/2018 8:33:47 AM

--- nameAge.py
+++ nameAge.py
@@ -1,1 +1,10 @@
+#Shaylee McBride
+#17Jan2018
+#nameAge.py - tells you stuff based on your name and age
 
+name = input("Enter your first and last name: ")
+age = int(input('Enter your age: '))
+firstName, lastName = name.split()    #splits a string
+print('Your first name has',len(firstName),'letters')  #len counts characters in a string
+print('Your last name has',len(lastName),'letters')
+print('Next year you will be',age+1,'years old')
