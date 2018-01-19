2a482e7a729882aab2cf3d50b0896b79030a680b
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-01-19 08:24:45 -0500

    Updated from Brython Server: 1/19/2018 8:24:44 AM

--- warmup1.py
+++ warmup1.py
@@ -1,1 +1,7 @@
+#Shaylee McBride
+#19Jan2018
+#warmup1.py - area of triangle
 
+base = float(input('Base of triangle: '))
+height = float(input('Height of triangle: '))
+print('The area of the triangle is',0.5*base*height)
