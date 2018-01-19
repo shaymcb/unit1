5b2bbd5d12bb816547d506eea99923f3d933a0a3
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-01-19 08:10:58 -0500

    Update tipCalculator.py

--- tipCalculator.py
+++ tipCalculator.py
@@ -2,6 +2,6 @@
 #17Jan2018
 #tipCalculator.py - calculates how much you should tip
 
-priceMeal = int(input('Price of meal ($): '))
-tipPercent = int(input('Percent to tip: '))
+priceMeal = float(input('Price of meal ($): '))
+tipPercent = float(input('Percent to tip: '))
 print('You should tip',round(priceMeal*tipPercent*0.01,2),'dollars')
