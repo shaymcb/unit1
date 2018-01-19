4d4ab4e06b12316750fcbd79da56ceebaa317dbf
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-01-19 08:09:50 -0500

    Updated from Brython Server: 1/19/2018 8:09:49 AM

--- pythagoreanDemo.py
+++ pythagoreanDemo.py
@@ -4,6 +4,6 @@
 
 from math import sqrt
 
-a = float(input('Enter side A: ')
-b = float(input('Enter side B: ')
+a = float(input('Enter side A: '))
+b = float(input('Enter side B: '))
 print('The hypotenuse is',sqrt(a**2 + b**2))
