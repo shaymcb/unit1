131f69de6e94a89522fcb3e452acd2bcf4002cba
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-01-19 08:31:50 -0500

    Updated from Brython Server: 1/19/2018 8:31:49 AM

--- slope.py
+++ slope.py
@@ -1,1 +1,11 @@
+#Shaylee McBride
+#19Jan2018
+#slope.py - finds slope given two points
 
+x1 = float(input('x1 = '))
+y1 = float(input('y1 = '))
+x2 = float(input('x2 = '))
+y2 = float(input('y2 = '))
+slope = (y2 - y1)/(x2 - x1)
+print("The slope is",slope)
+print('The equation of the line is y =',slope,'x +',y1-slope*x1)
