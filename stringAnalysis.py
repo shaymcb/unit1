2e808fe9a37a91c3717812476458519ea7073b82
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-01-19 10:11:33 -0500

    Updated from Brython Server: 1/19/2018 10:11:31 AM

--- stringAnalysis.py
+++ stringAnalysis.py
@@ -1,1 +1,9 @@
+#Shaylee McBride
+#19Jan2018
+#stringAnalysis.py - reports words and characters in a string
 
+sentence = input('Enter a sentence: ')
+words = sentence.count(" ") + 1
+print("Your sentence has",words, "words,",len(sentence),"characters and",len(sentence) - sentence.count(" "),"letters.")
+letter = input("Enter a character to search for: ")
+print("Your sentence has",sentence.count(letter),"occurences of the character",letter)
