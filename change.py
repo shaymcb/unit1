76721b60495bb00c333e732ecbb5c49b69942090
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-01-22 12:54:35 -0500

    Update change.py

--- change.py
+++ change.py
@@ -3,9 +3,9 @@
 #change.py - how many of each coin you need to meet cents
 
 cents = int(input("Input a number in cents: "))
-quarters = int(cents/25)
-dimes = int((cents - quarters * 25)/10)
-nickles = int((cents - quarters * 25 - dimes * 10)/5)
+quarters = cents//25
+dimes = (cents - quarters * 25)//10
+nickles = (cents - quarters * 25 - dimes * 10)//5
 pennies = int(cents - quarters * 25 - dimes * 10 - nickles * 5)
 print('Quarters: ', quarters)
 print("Dimes: ",dimes)
