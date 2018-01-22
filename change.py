2aedc88f5e437fc85edd8d71c912de3618e95301
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-01-22 08:31:14 -0500

    Updated from Brython Server: 1/22/2018 8:31:13 AM

--- change.py
+++ change.py
@@ -1,1 +1,13 @@
+#Shaylee McBride
+#22Jan2018
+#change.py
 
+cents = int(input("Input a number in cents: "))
+quarters = int(cents/25)
+dimes = int((cents - quarters * 25)/10)
+nickles = int((cents - quarters * 25 - dimes * 10)/5)
+pennies = int(cents - quarters * 25 - dimes * 10 - nickles * 5)
+print('Quarters: ', quarters)
+print("Dimes: ",dimes)
+print("Nickles: ",nickles)
+print('Pennies: ',pennies)
