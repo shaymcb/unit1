d2b33cb3b0cce2cd02e6a7030f33d2478dad4e2d
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-01-22 12:53:15 -0500

    Updated from Brython Server: 1/22/2018 12:53:14 PM

--- binary.py
+++ binary.py
@@ -3,20 +3,19 @@
 #binary.py - converts binary to base-10
 
 binary = int(input("Input an 8-digit binary number: "))
-b128 = int(binary/10000000)
+b128 = binary//10000000
 binary = binary - b128 * 10000000
-b64 = int(binary/1000000)
+b64 = binary//1000000
 binary = binary - b64 * 1000000
-b32 = int(binary/100000)
+b32 = binary//100000
 binary = binary - b32 * 100000
-b16 = int(binary/10000)
+b16 = binary//10000
 binary = binary - b16 * 10000
-b8 = int(binary/1000)
+b8 = binary//1000
 binary = binary - b8 * 1000
-b4 = int(binary/100)
+b4 = binary//100
 binary = binary - b4 * 100
-b2 = int(binary/10)
-binary = binary - b2 * 10
-b1 = int(binary)
+b2 = binary//10
+b1 = binary - b2 * 10
 base10 = b128*128+b64*64+b32*32+b16*16+b8*8+b4*4+b2*2+b1
 print(base10)
