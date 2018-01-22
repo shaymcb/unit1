c62c233cd6ff6c43a9509fb7f6ede883d17191b2
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-01-22 08:16:28 -0500

    Updated from Brython Server: 1/22/2018 8:16:28 AM

--- additionGameDemo.py
+++ additionGameDemo.py
@@ -6,5 +6,5 @@ from random import randint
 
 num1 = randint(-10,10)  #pseudorandom, not truly random
 num2 = randint(-10,10)
-answer = int(input(num1, "+", num2, "= "))
+answer = int(input(str(num1)+" + "+str(num2)+' = '))  #connect strings w +, call var strings
 print(answer == num1 + num2)
