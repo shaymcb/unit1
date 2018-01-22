fa3fd8575b6b657ed6f43ffb74d51a1bf0178798
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-01-22 08:43:15 -0500

    Updated from Brython Server: 1/22/2018 8:43:15 AM

--- isItATriangle.py
+++ isItATriangle.py
@@ -1,1 +1,11 @@
+#Shaylee McBride
+#22Jan2018
+#isItATriangle.py - is it a triangle?
 
+sideA = float(input("Enter Side A: "))
+sideB = float(input("Enter Side B: "))
+sideC = float(input("Enter Side C: "))
+bigside = max(sideA,sideB,sideC)
+smallside = min(sideA,sideB,sideC)
+midside = sideA + sideB + sideC - bigside - smallside
+print(midside + smallside < bigside)
