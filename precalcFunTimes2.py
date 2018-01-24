24f687f78748ab081b49ee2764114b7186b8fca5
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-01-24 08:39:10 -0500

    Updated from Brython Server: 1/24/2018 8:39:10 AM

--- precalcFunTimes2.py
+++ precalcFunTimes2.py
@@ -1,1 +1,9 @@
+#Shaylee McBride
+#24Jan2018
+#precalcFunTimes2.py - ah fuck it's compounding interest
+
+from math import log10
+
+principal = float(input('Enter starting amount of money: ')
+interestRate = float
 
