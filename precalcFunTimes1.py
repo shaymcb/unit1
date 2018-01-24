16ad9aed92af4a3d005d78c9c9edad5ceba3cd10
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-01-24 08:35:43 -0500

    Updated from Brython Server: 1/24/2018 8:35:43 AM

--- precalcFunTimes1.py
+++ precalcFunTimes1.py
@@ -1,1 +1,11 @@
+#Shaylee McBride
+#24Jan2018
+#precalcFunTimes1.py - computes the distance between two points
 
+from math import sqrt
+
+x1 = float(input('Enter x1: '))
+y1 = float(input('Enter y1: '))
+x2 = float(input('Enter x2: '))
+y2 = float(input('Enter y2: '))
+print('The distance is',sqrt((x1-x2)**2+(y1-y2)**2))
